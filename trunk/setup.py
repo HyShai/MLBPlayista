69fefd0a6f13bafee1ea97c0b0b6821bf67d4796
jesserosenthal <jesserosenthal@d37ac8f2-af4a-0410-91f3-abd50a2159f9>
2008-04-15 01:16:43 +0000

    Added setup script.

---
+++ trunk/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name = "mlbviewer",
+      version = "0.1alpha5",
+      description = "Tools to watch and listen to baseball on MLB.tv",
+      author = "Jesse Rosenthal, Matthew D. Aftcat",
+      author_email = "jesse.k.rosenthal@gmail.com",
+      url = "http://sourceforge.net/projects/mlbviewer/",
+      packages = ['MLBviewer'],
+      scripts = ['mlbviewer.py'])
+      
