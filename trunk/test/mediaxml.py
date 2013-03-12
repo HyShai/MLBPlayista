826c21ddf2ea8cd82e758d8131a1917d80c9d20a
daftcat75 <daftcat75@d37ac8f2-af4a-0410-91f3-abd50a2159f9>
2013-03-12 14:49:09 +0000

    added use_wired_web config to enable basic users to select nexdef streams

--- trunk/test/mediaxml.py
+++ trunk/test/mediaxml.py
@@ -1,62 +1,31 @@
 #!/usr/bin/env python
- 
-# $Revision$
 
-import os.path
+from xml.dom import *
+from xml.dom.minidom import *
 import sys
-import re
-
-import logging
-from xml.dom.minidom import parse
-
-FIELDS = ( 'event-id', 'content-id', 'type', 'state', 'cat-code', 
-           'playback-scenario', 'login-required', 'auth-required', 
-           'innings-index', 'session-key', 'url', 'status-code',
-           'status-message', 'postal-code', 'blackout')
-
-OTHERS = ( 'blackout-status', 'auth-status' )
 
 try:
-    XMLFILE = sys.argv[1]
+  xmlfile = sys.argv[1]
 except:
-    print "Please specify an xml file to parse."
-    sys.exit(1)
+  print "%s: Please specify an xml filename to parse" % sys.argv[0]
+  sys.exit()
 
-xp = parse(XMLFILE)
+try:
+  xp = parse(xmlfile)
+except:
+  print "%s %s: Could not parse xmlfile." % (sys.argv[0], xmlfile)
+  sys.exit()
 
-errorReply = False
-uve = len(xp.getElementsByTagName('user-verified-event'))
+IL = 0
 
-if uve != 1:
-    print "No <user-verified-event> found.  This must be an error reply."
-    errorReply = True
+def printChildNodes(node,IL):
+  if node.hasChildNodes():
+    print "%s %s:" % (IL*' ', node.nodeName)
+    IL += 1
+    for child in node.childNodes:
+      printChildNodes(child,IL)
+  else:
+    print "%s %s: %s" % (IL*' ', node.nodeName, node.nodeValue)
+    IL -= 1
 
-reply = {}
-for elem in FIELDS:
-    try:
-        reply[elem] = xp.getElementsByTagName(elem)[0].childNodes[0].data 
-    except:
-        print "could not find elem: " + elem
-
-for elem in OTHERS:
-    try:
-        tmp = xp.getElementsByTagName(elem)[0].childNodes[0]
-    except:
-        reply[elem] = ''
-    else:
-        reply[elem] = tmp.tagName
-
-dattrs = {}
-for da in xp.getElementsByTagName('domain-attribute'):
-    name = da.getAttribute('name')
-    dattrs[name] = da.childNodes[0].data
-
-print 
-for key in FIELDS + OTHERS:
-    if reply.has_key(key):
-        print key + ' = ' + reply[key] + '\n'
-
-print '<<< DOMAIN ATTRIBUTES >>> \n'
-for key in dattrs.keys():
-    print key + ' = ' + dattrs[key] + '\n'
-
+printChildNodes(xp,IL)
