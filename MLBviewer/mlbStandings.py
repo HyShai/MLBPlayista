8236dbba99780d08903c6e8c050de8ba69bb3364
daftcat75 <daftcat75@d37ac8f2-af4a-0410-91f3-abd50a2159f9>
2013-04-20 01:48:00 +0000

    more tmp checkins - do not use yet
    git-svn-id: https://svn.code.sf.net/p/mlbviewer/code/trunk@447 d37ac8f2-af4a-0410-91f3-abd50a2159f9

--- MLBviewer/mlbStandings.py
+++ MLBviewer/mlbStandings.py
@@ -10,15 +10,10 @@ class MLBStandings:
     def __init__(self):
         self.data = []
         self.xml = ""
-        # For development purposes, let's parse from a file (activate web
-        # code later)
-        f = open('standings.xml')
-        self.xml = f.read()
-        f.close()
         self.url = 'https://erikberg.com/mlb/standings.xml'
 
         # some decoder strings from sportsML to plain-text
-        DIVISIONS={
+        self.DIVISIONS={
             'MLB.AL.E':  'AL East',
             'MLB.AL.C':  'AL Central',
             'MLB.AL.W':  'AL West',
@@ -27,8 +22,22 @@ class MLBStandings:
             'MLB.NL.W':  'NL West',
         }
 
+    def getDataFromFile(self):
+        # For development purposes, let's parse from a file (activate web
+        # code later)
+        f = open('standings.xml')
+        self.xml = f.read()
+        f.close()
 
-    def getStandingsData(self):
+    def getStandingsData(self,offline=True):
+        # To limit test requests until permission has been obtained
+        # from data provider
+        if offline:
+            print "Offline is True, using local file"
+            self.getDataFromFile()
+            self.parseStandingsXml()
+            return
+        #print "Offline is False, requesting from network"
         request = urllib2.Request(self.url)
         request.add_header('Accept-encoding', 'gzip')
         request.add_header('User-agent', 'mlbviewer/2013sf3 https://sourceforge.net/projects/mlbviewer/   (straycat000@yahoo.com)')
@@ -38,59 +47,65 @@ class MLBStandings:
         compressedStream = StringIO.StringIO(compressedData)
         gzipper = gzip.GzipFile(fileobj=compressedStream)
         self.xml = gzipper.read()
+        self.parseStandingsXml()
 
     def parseStandingsXml(self):
         xp = parseString(self.xml)
+        for metadata in xp.getElementsByTagName('sports-metadata'):
+            self.last_update = metadata.getAttribute('date-time')
         for standing in xp.getElementsByTagName('standing'):
-            for div in standing.getAttribute('sports-content-code'):
+            for div in standing.getElementsByTagName('sports-content-code'):
                 type=div.getAttribute('code-type')
-                if type == 'division':
+                if type == "division":
                     key = div.getAttribute('code-key')
-                    division = DIVISIONS[key] 
-                    self.parseDivisionData(standing)
+                    division = self.DIVISIONS[key] 
+            self.data.append((division,self.parseDivisionData(standing)))
 
     def parseDivisionData(self,xp):
         out = []
         for tptr in xp.getElementsByTagName('team'):
             out.append(self.parseTeamData(tptr))
+        return out
 
 
     def parseTeamData(self,tptr):
-            for name in tptr.getElementsByTagName('name'):
-                tmp['first'] = name.getAttribute('first')
-                tmp['last']  = name.getAttribute('last')
-            for teamStats in tptr.getElementsByTagName('team-stats'):
-                tmp['G'] = teamStats.getAttribute('events-played')
-                tmp['GB'] = teamStats.getAttribute('games-back')
-                for totals in teamStats.getElementsByTagName('outcome-totals'):
-                    scope = totals.getAttribute('alignment-scope')
-                    if scope == "events-all":
-                        tmp['W'] = totals.getAttribute('wins')
-                        tmp['L'] = totals.getAttribute('losses')
-                        tmp['WP'] = totals.getAttribute('winning-percentage')
-                        streak = totals.getAttribute('streak-type')
-                        if streak == 'win':
-                            tmp['STRK'] = 'W'
-                        else:
-                            tmp['STRK'] = 'L'
-                        tmp['STRK'] += str(totals.getAttribute('streak-total')
-                        tmp['RS'] = totals.getAttribute('points-scored-for')
-                        tmp['RA'] = totals.getAttribute('points-scored-against')
-                    elif scope == "events-home":
-                        tmp['HW'] = totals.getAttribute('wins')
-                        tmp['HL'] = totals.getAttribute('losses')
-                    elif scope == "events-away":
-                        tmp['AW'] = totals.getAttribute('wins')
-                        tmp['AL'] = totals.getAttribute('losses')
-                    elif scope == "":
-                        scope = totals.getAttribute('duration-scope')
-                        if scope == 'events-most-recent-5':
-                            tmp['L5_W'] = totals.getAttribute('wins')
-                            tmp['L5_L'] = totals.getAttribute('losses')
-                        elif scope == 'events-most-recent-10':
-                            tmp['L10_W'] = totals.getAttribute('wins')
-                            tmp['L10_L'] = totals.getAttribute('losses')
-                            
+        tmp = dict()
+        for name in tptr.getElementsByTagName('name'):
+            tmp['first'] = name.getAttribute('first')
+            tmp['last']  = name.getAttribute('last')
+        for teamStats in tptr.getElementsByTagName('team-stats'):
+            tmp['G'] = teamStats.getAttribute('events-played')
+            tmp['GB'] = teamStats.getAttribute('games-back')
+            for totals in teamStats.getElementsByTagName('outcome-totals'):
+                scope = totals.getAttribute('alignment-scope')
+                if scope == "events-all":
+                    tmp['W'] = totals.getAttribute('wins')
+                    tmp['L'] = totals.getAttribute('losses')
+                    tmp['WP'] = totals.getAttribute('winning-percentage')
+                    streak = totals.getAttribute('streak-type')
+                    if streak == 'win':
+                        tmp['STRK'] = 'W'
+                    else:
+                        tmp['STRK'] = 'L'
+                    tmp['STRK'] += str(totals.getAttribute('streak-total'))
+                    tmp['RS'] = totals.getAttribute('points-scored-for')
+                    tmp['RA'] = totals.getAttribute('points-scored-against')
+                elif scope == "events-home":
+                    tmp['HW'] = totals.getAttribute('wins')
+                    tmp['HL'] = totals.getAttribute('losses')
+                elif scope == "events-away":
+                    tmp['AW'] = totals.getAttribute('wins')
+                    tmp['AL'] = totals.getAttribute('losses')
+                elif scope == "":
+                    scope = totals.getAttribute('duration-scope')
+                    if scope == 'events-most-recent-5':
+                        tmp['L5_W'] = totals.getAttribute('wins')
+                        tmp['L5_L'] = totals.getAttribute('losses')
+                    elif scope == 'events-most-recent-10':
+                        tmp['L10_W'] = totals.getAttribute('wins')
+                        tmp['L10_L'] = totals.getAttribute('losses')
+        return tmp
                         
-
                     
+
+                
