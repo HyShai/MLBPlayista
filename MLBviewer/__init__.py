6a0922c3d96b13a2e0b0a7da9b417881fb180b03
daftcat75 <daftcat75@d37ac8f2-af4a-0410-91f3-abd50a2159f9>
2013-05-14 07:32:04 +0000

    revved version to sf4
    git-svn-id: https://svn.code.sf.net/p/mlbviewer/code/trunk@476 d37ac8f2-af4a-0410-91f3-abd50a2159f9

--- MLBviewer/__init__.py
+++ MLBviewer/__init__.py
@@ -3,7 +3,7 @@
 __author__ = "Matthew Levine"
 __email__ = "straycat000@yahoo.com"
 
-VERSION ="2013rev450+"
+VERSION ="2013sf4"
 URL = "http://sourceforge.net/projects/mlbviewer"
 
 AUTHDIR = '.mlb'
