6d9ebdc06a261cbbc0996c52520d048f8b41d92d
daftcat75 <daftcat75@d37ac8f2-af4a-0410-91f3-abd50a2159f9>
2013-05-20 20:58:14 +0000

    revved version back to svn version

--- trunk/MLBviewer/mlbConstants.py
+++ trunk/MLBviewer/mlbConstants.py
@@ -13,7 +13,7 @@
 # Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
 # 02111-1307 USA
 
-VERSION ="2013-sf-6"
+VERSION ="2013rev492+"
 URL = "http://sourceforge.net/projects/mlbviewer"
 EMAIL = "straycat000@yahoo.com"
 
